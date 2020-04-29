cccc25f59736500740d151cb331cff0dcffe1f20
Niraj Tiwari <tniraj7@gmail.com>
2020-04-29 22:38:41 +0900

    populate detail fragment with the animal argument

--- Animals/app/src/main/java/com/example/animals/view/DetailFragment.kt
+++ Animals/app/src/main/java/com/example/animals/view/DetailFragment.kt
@@ -9,9 +9,15 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 
 import com.example.animals.R
+import com.example.animals.model.Animal
+import com.example.animals.utils.getProgressDrawable
+import com.example.animals.utils.loadImage
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.item_animal.*
 
 class DetailFragment : Fragment() {
+
+    var animal: Animal? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +25,22 @@ class DetailFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            animal = DetailFragmentArgs.fromBundle(it).animal
+        }
+
+        context?.let {
+            animalImageView.loadImage(animal?.imageUrl, getProgressDrawable(it))
+        }
+
+        animalLifeSpan.text = animal?.lifeSpan
+        animalDiet.text = animal?.diet
+        animalLocation.text = animal?.location
+        animalNameText.text = animal?.name
     }
 }
