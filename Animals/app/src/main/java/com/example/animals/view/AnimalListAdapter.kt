94b5fe8a633f8caf5c8c483ea597ab83029de6cf
Niraj Tiwari <tniraj7@gmail.com>
2020-04-29 22:37:54 +0900

    add click listener to animal item view in recycler view adapter to navigate to details fragment

--- Animals/app/src/main/java/com/example/animals/view/AnimalListAdapter.kt
+++ Animals/app/src/main/java/com/example/animals/view/AnimalListAdapter.kt
@@ -5,11 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animals.R
 import com.example.animals.model.Animal
 import com.example.animals.utils.getProgressDrawable
 import com.example.animals.utils.loadImage
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_animal.view.*
 
 
 class AnimalListAdapter( private val animalsList: ArrayList<Animal>):
@@ -28,17 +35,24 @@ class AnimalListAdapter( private val animalsList: ArrayList<Animal>):
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindView(animalsList[position])
+
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         fun bindView(animal: Animal) {
 
+            val animalLayout = itemView.findViewById<ConstraintLayout?>(R.id.animalLayout)
             val animalImageView = itemView.findViewById<ImageView?>(R.id.animalImage)
             val animalText = itemView.findViewById<TextView?>(R.id.animalName)
 
             animalImageView?.loadImage(animal.imageUrl, getProgressDrawable(itemView.context))
             animalText?.text = animal.name
+
+            animalLayout?.setOnClickListener {
+                val action = ListFragmentDirections.actionDetail(animal)
+                Navigation.findNavController(itemView).navigate(action)
+            }
         }
     }
 
