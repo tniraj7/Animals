09ff4267ccb353e517094081a1c788358fe07354
Niraj Tiwari <tniraj7@gmail.com>
2020-05-03 21:40:57 +0900

    provide 2 difference SharedPrefHelper for different context from App or Activity using Qualifier annotation

--- Animals/app/src/main/java/com/example/animals/di/PrefsModule.kt
+++ Animals/app/src/main/java/com/example/animals/di/PrefsModule.kt
@@ -1,9 +1,11 @@
 package com.example.animals.di
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
 import com.example.animals.utils.SharedPreferencesHelper
 import dagger.Module
 import dagger.Provides
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -11,7 +13,21 @@ class PrefsModule {
 
     @Provides
     @Singleton
+    @TypeOfContext(CONTEXT_APP)
     fun provideSharedPreferences(app: Application): SharedPreferencesHelper {
         return SharedPreferencesHelper(app)
     }
+
+    @Provides
+    @Singleton
+    @TypeOfContext(CONTEXT_ACTIVIY)
+    fun provideActivitySharedPreferences(activity: AppCompatActivity): SharedPreferencesHelper {
+        return SharedPreferencesHelper(activity)
+    }
 }
+
+const val CONTEXT_APP = "Application Context"
+const val CONTEXT_ACTIVIY = "Acitivity Context"
+
+@Qualifier
+annotation class TypeOfContext(val type: String)
